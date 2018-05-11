6785fcb2ce1bd8dc3fb485ede9cfe22c5dfc34f2
haslam <haslam414@gmail.com>
2018-05-11 20:28:35 +0100

    hex to rgb on keydown on input

--- hex-rgb.js
+++ hex-rgb.js
@@ -1,10 +1,25 @@
+//Input fields
+const hexInput = document.querySelector('.hex-input');
+const rgbInput = document.querySelector('.rgb-input');
+
+//Array index for color hex codes
+const index = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
+//Key press keyCodes array
+const keyCodes = ['48','49','50','51','52','53','54','55','56','57','65','66','67','68','69','70'];
+
+ let hexCode = [];
+ 
+//keydown event fired on every keyed button
+hexInput.addEventListener('keydown', converter);
+
+
 /**
 * hex to array conversion
-* @param {String} hex Hex of a color
-* @returns {Array} 
+* @param {String} hex Hex of a color 
 *
 */
-const index = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
 function hexConversion(hex) {
     
    
@@ -13,31 +28,45 @@ function hexConversion(hex) {
             return false;
         }
     
+    //split hex into an string array
     let hexArr = hex.split("");
     
-    let newArr = [];
+    let newHexArr = [];
     
     for(let i=0; i<hexArr.length; i++) {
+        
         let xter = hexArr[i].toUpperCase();
+        
+        //match each hex character with the index array above go validate keyed ones
         let hexIndex = inArray(xter, index);
         
-        if (hexIndex) {
-            newArr.push(hexIndex);
-        }
+        //check for true and number zero as some hex have zero in them
+        if (hexIndex || hexIndex === 0) {
+            newHexArr.push(hexIndex);
+        
+        }     
         
     }
+
+    let rgbColorCode = makeRGB(newHexArr); 
     
-    makeRGB(newArr);
-   
+    return rgbColorCode;
 }
 
-//returns the index of an array value if found matching a specified value
+/**
+* Returns the index of an array value if found matching a specified value
+* @param {String} needle The matching character to compare
+* @param {Array} stack The array to find character in
+* @return {Number} The index of the matching character in the array
+*/
+    
 function inArray(needle, stack) {
-    if (typeof stack === 'undefined') {
+    if (typeof stack === 'undefined' || typeof needle === 'undefined') {
         return false;
     }
     let stackLen = stack.length;
     for (let i=0; i < stackLen; i++) {
+        
           if (stack[i] == needle) {
             let stackIndex = i;
             return stackIndex;
@@ -46,6 +75,11 @@ function inArray(needle, stack) {
     return false;
 }
 
+/**
+* make RGB value out of the hex index array
+* @param {Array} rgb An array of hex index
+* @return {String} rgbVal Converted RGB value 
+*/
 function makeRGB(rgb) {
     if (typeof rgb === 'undefined') { return false; }
     //get the 2 left digits of the hex value for red
@@ -69,8 +103,37 @@ function makeRGB(rgb) {
         rgbArray.push(rgbConvert);
     }
     
-     console.log(rgbArray);
+    return rgbArray.toString();
+    
 }
 
-const white = "ffffff";
-hexConversion(white);
+ 
+function converter(e) {
+    let rgbColor = [];
+    rgbInput.value = '';
+    const backspace = 8;
+    
+    let inputKeyCode = inArray(e.keyCode, keyCodes);
+    
+    if (inputKeyCode || inputKeyCode === 0) {
+        hexCode.push(e.key);
+        let colorCode = hexCode.join('');
+        document.body.style.backgroundColor = `#${colorCode}`;
+        
+        if(colorCode.length === 6) {
+            rgbColor = hexConversion(colorCode);
+            rgbInput.value = '(' + rgbColor +')';            
+        }
+        
+        if(colorCode.length > 6) {
+            return false;
+        }
+    }
+    
+    if (e.keyCode == backspace) {
+        hexCode.pop();
+        rgbInput.value = '';
+    }
+
+    
+}
